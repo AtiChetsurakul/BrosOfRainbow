f85591a5b3f18f8124aacc5717b696eb70340a86
Todsavad Tangtortan <guntsvzz@gmail.com>
2023-05-06 10:53:09 +0700

    Update model_rainbow_lstm.py

--- python script/model_rainbow_lstm.py
+++ python script/model_rainbow_lstm.py
@@ -18,7 +18,7 @@ class Network_lstm(nn.Module):
         self.out_dim = out_dim
         self.atom_size = atom_size
 
-# Accroding to paper
+    # Accroding to paper
         self.feature_layer = nn.Sequential(
         nn.Conv2d(in_channels=4, out_channels=32, kernel_size=8, stride=4),
         nn.ReLU(),
@@ -26,12 +26,15 @@ class Network_lstm(nn.Module):
         nn.ReLU(),
         nn.Conv2d(in_channels=512, out_channels=64, kernel_size=3, stride=1),
         nn.ReLU(),
-        nn.Flatten(),
-        nn.Linear(3136, 128), # Output shape is [64, out_dim] 
-        nn.ReLU(),
+        nn.Flatten()
         )
 
-        self.lstm = nn.LSTM(input_size=128, hidden_size=128, batch_first=True)
+        self.lstm = nn.LSTM(input_size=64, hidden_size=32, batch_first=True)
+
+        self.feature_layer = nn.Sequential(
+            nn.Linear(32, 128), # Output shape is [64, out_dim] 
+            nn.ReLU(),
+        )
 
         # set advantage layer
         self.advantage_hidden_layer = NoisyLinear(128, 128, std_init=0.5)
@@ -51,8 +54,11 @@ class Network_lstm(nn.Module):
     def dist(self, x: torch.Tensor, hidden = None) -> torch.Tensor:
         """Get distribution for atoms."""
         feature = self.feature_layer(x)
-        feature, hidden = self.lstm(feature, hidden)
-        
+        print(feature.shape)
+
+        feature, hidden = self.lstm(feature.unsqueeze(1), hidden)
+        feature = self.feature_layer2(x)
+
         adv_hid = F.relu(self.advantage_hidden_layer(feature))
         val_hid = F.relu(self.value_hidden_layer(feature))
         
